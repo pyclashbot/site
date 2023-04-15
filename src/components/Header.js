95eb27f573148b9e065dd80ffc6c2216dba80325
Martin Miglio <marmig0404@gmail.com>
2023-04-14 20:52:10 -0400

    remove asset prefix

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,6 @@
 import React, { Component } from "react";
 import Head from "next/head";
 
-const isProd = process.env.NODE_ENV === "production";
-const assetPrefix = isProd ? "/py-clash-bot" : "";
-
 /**
  * Header Component
  */
@@ -28,7 +25,7 @@ export default class Header extends Component {
     return (
       <Head>
         <title>{this.state.title}</title>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta name="description" content={this.state.description} />
         <meta name="keywords" content={this.state.keywords} />
         <meta property="og:type" content="website" />
@@ -41,24 +38,24 @@ export default class Header extends Component {
         <meta property="twitter:title" content={this.state.title} />
         <meta property="twitter:description" content={this.state.description} />
         <meta httpEquiv="Cache-Control" content="max-age=86400" />
-        <meta http-equiv="Permissions-Policy" content="interest-cohort=()" />
+        <meta httpEquiv="Permissions-Policy" content="interest-cohort=()" />
         <meta
           name="google-site-verification"
           content="Fimwu5S-RkDYvpJD2yVaGoZYT8ticA_v_V285-5AFqg"
         />
         <link rel="canonical" href={this.state.canonicalUrl} />
-        <link rel="icon" href={assetPrefix + "/favicon.ico"} />
+        <link rel="icon" href="/favicon.ico" />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href={assetPrefix + "/favicon-32x32.png"}
+          href="/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href={assetPrefix + "/favicon-16x16.png"}
+          href="/favicon-16x16.png"
         />
       </Head>
     );
