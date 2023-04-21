856067c3935eb16daff1672b5335ead459b38090
Martin Miglio <marmig0404@gmail.com>
2023-04-21 02:32:41 -0400

    update CMS to render other markdown pages

--- src/components/cms/MarkdownLink.js
+++ src/components/cms/MarkdownLink.js
@@ -2,6 +2,13 @@ import React from "react";
 import { handleClick } from "../../GoogleAnalytics";
 
 const MarkdownLink = ({ href, children }) => {
+  // if href links to .md file, it should really link to the page
+  if (href.includes(".md")) {
+    const fileName = href.split("/").pop();
+    const pageName = fileName.replace(".md", "");
+    const pageNameLower = pageName.toLowerCase();
+    href = `/${pageNameLower}`;
+  }
   return (
     <a
       href={href}
