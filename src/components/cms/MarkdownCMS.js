693f74292dc29e6e876559f5ba9be824d2ed5997
Martin Miglio <marmig0404@gmail.com>
2023-04-22 14:11:05 -0400

    remove old comments

--- src/components/cms/MarkdownCMS.js
+++ src/components/cms/MarkdownCMS.js
@@ -1,17 +1,10 @@
-// this compnent is a content management system for reading the readme file from the github repo
-// it is used to generate the website from the readme file, replacing select elements with custom components
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import dynamic from "next/dynamic";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 import styles from "./Markdown.module.css";
 
-// import MarkdownLink from "./MarkdownLink";
-// import MarkdownBody from "./MarkdownBody";
-// import { MarkdownHeader, MarkdownSubheader } from "./MarkdownHeader";
-// import components above dynamically to prevent build errors
 const MarkdownLink = dynamic(() => import("./MarkdownLink"));
 const MarkdownBody = dynamic(() => import("./MarkdownBody"));
 const MarkdownHeader = dynamic(() => import("./MarkdownHeader"));
